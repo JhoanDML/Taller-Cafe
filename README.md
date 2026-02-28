f3c922f8734b55c316207b272a401d404d2a0cce
JhoanDML <jhoanmalua20@gmail.com>
2026-02-27 20:26:15 -0500

    Inicio del proyecto

---
+++ README.md
@@ -0,0 +1,49 @@
+#  Simple Coffee Listing
+
+Proyecto frontend desarrollado con **React + Vite**, basado en el challenge de **devChallenges**, que consume una API externa y muestra un catálogo de cafés con filtros diseño responsive.
+
+---
+
+## Integrantes
+Jhoan David Malua Luna
+
+
+
+##  Capturas
+
+### 💻 Vista Desktop
+
+![Desktop Preview](./screenshots/compu.png)
+
+### 📱 Vista Mobile
+
+![Mobile Preview](./screenshots/celu.png)
+
+
+
+## 🛠 Tecnologías
+
+-  React
+-  Vite
+-  CSS puro
+-  Fetch API
+-  Arquitectura por componentes
+
+---
+
+## Instalacion y Ejecucion
+
+1. Clonar el repositorio localmente
+- https://github.com/JhoanDML/Taller-Cafe
+2. Instalar dependencias 
+- npm install
+3. Ejecutar en desarrollo 
+- npm run dev
+4. Build de producción
+- npm run build
+5. API utilizada
+- https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/simple-coffee-listing-data.json
+
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import CoffeeList from './components/CoffeeList'
+import FilterBar from './components/FilterBar'
+import SkeletonCard from './components/SkeletonCard'
+
+function App() {
+  const [coffee, setCoffee] = useState([])
+  const [filter, setFilter] = useState('all')
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch(
+    )
+      .then(res => {
+        if (!res.ok) throw new Error('Error loading data')
+        return res.json()
+      })
+      .then(data => {
+        setCoffee(data)
+        setLoading(false)
+      })
+      .catch(err => {
+        setError(err.message)
+        setLoading(false)
+      })
+  }, [])
+
+  const filteredCoffee =
+    filter === 'available'
+      ? coffee.filter(c => c.available)
+      : coffee
+
+  return (
+    <main className="container">
+      <h1>Our Collection</h1>
+
+      <p>
+        Introducing our Coffee Collection, a selection of unique coffees
+        from different roast types and origins.
+      </p>
+
+      <FilterBar filter={filter} setFilter={setFilter} />
+
+      {loading && (
+        <div className="grid">
+          {[...Array(6)].map((_, i) => (
+            <SkeletonCard key={i} />
+          ))}
+        </div>
+      )}
+
+      {error && <p style={{ textAlign: 'center' }}>{error}</p>}
+
+      {!loading && !error && (
+        <CoffeeList coffee={filteredCoffee} />
+      )}
+    </main>
+  )
+}
+
+export default App
